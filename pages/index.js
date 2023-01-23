ad6245e280de5807bd2b398b147ce87cdeffe476
Dilkash <dilkashpeshimam@gmail.com>
2023-01-23 21:47:08 +0530

    linking

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,15 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Navbar from '@/components/Navbar/Navbar'
 import Footer from '@/components/Footer/Footer'
-
-// const inter = Inter({ subsets: ['latin'] })
-
+import Link
+  from 'next/link'
 export default function Home() {
   return (
     <div>
       <Navbar />
       <h1>Home Page</h1>
       <p>This is home page</p>
+      <Link href='alldata'>All Data</Link>
       <Footer />
     </div>
   )
