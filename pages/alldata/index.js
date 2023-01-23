400767cbe6dc706afaaa40c7a68778e0d4097c71
Dilkash <dilkashpeshimam@gmail.com>
2023-01-23 21:24:33 +0530

    created routing & components

---
+++ pages/alldata/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AllData = () => {
+    return (
+        <div><h1>All data</h1></div>
+    )
+}
+
+export default AllData
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,18 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import Navbar from '@/components/Navbar/Navbar'
+import Footer from '@/components/Footer/Footer'
 
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home Page</h1>
       <p>This is home page</p>
+      <Footer />
     </div>
   )
 }
