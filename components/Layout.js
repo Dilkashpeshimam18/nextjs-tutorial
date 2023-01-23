ca321dc966506faf9477c80418f45169d187dae1
Dilkash <dilkashpeshimam@gmail.com>
2023-01-23 21:56:53 +0530

    created layout component

---
+++ components/Layout.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar/Navbar";
+import Footer from "./Footer/Footer";
+const Layout = ({ children }) => {
+    return (<>
+        <Navbar />
+        {children}
+        <Footer />
+
+    </>);
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,9 @@ import Link
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Home Page</h1>
       <p>This is home page</p>
       <Link href='alldata'>All Data</Link>
-      <Footer />
     </div>
   )
 }
