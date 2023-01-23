c87ee70878f18353f424767af6e52e9b219fb08d
Dilkash <dilkashpeshimam@gmail.com>
2023-01-23 21:40:22 +0530

    added linking between pages

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,7 +1,12 @@
-
+import Link from "next/link"
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div>
+            <Link href='/'>Home</Link>
+            <Link href='/alldata'>All Data</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/contact'>Contact</Link>
+        </div>
     )
 }
 
