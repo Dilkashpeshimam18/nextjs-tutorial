70f50aecf74a00321f9aafdc70f10a44f3320f6d
Dilkash <dilkashpeshimam@gmail.com>
2023-01-24 15:08:45 +0530

    fetching data from api using getStaticProps

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
     return (
         <div>
             <Link href='/'>Home</Link>
-            <Link href='/alldata'>All Data</Link>
+            <Link href='/alldata'>All Users</Link>
             <Link href='/about'>About</Link>
             <Link href='/contact'>Contact</Link>
         </div>
--- pages/alldata/index.js
+++ pages/alldata/index.js
@@ -2,15 +2,35 @@ import React from 'react'
 import Image from 'next/image'
 import Head from 'next/head'
 
-const AllData = () => {
+
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+
+    return {
+        props: {
+            users: data
+        }
+    }
+}
+
+const AllData = ({ users }) => {
     return (
         <>
             <Head>
-                <title>Next Tutorial | All Data </title>
+                <title>Next Tutorial | All User </title>
 
             </Head>
-            <div><h1>All data</h1>
+            <div><h1>All User</h1>
                 <Image src='/img.png' width={200} height={200} />
+                {users.map((user) => {
+                    return (
+                        <div key={user.id}>
+                            <h6>{user.name}</h6>
+                            <p>{user.email}</p>
+                        </div>
+                    )
+                })}
             </div>
         </>
 
